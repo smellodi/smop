2922600127a65abea5e60703febb27ac32bc87a5
lexasss <oleg.spakov@tuni.fi>
2024-01-24 14:36:58 +0200

    ML client/server refactored

--- smop.ml/TcpServer.cs
+++ smop.ml/TcpServer.cs
@@ -2,7 +2,6 @@ using Smop.Common;
 using System;
 using System.Text;
 using System.Threading.Tasks;
-//using WatsonTcp;
 using Tcp.NET.Server;
 using Tcp.NET.Server.Models;
 using Tcp.NET.Server.Events.Args;
@@ -14,6 +13,7 @@ public enum Status { Disconnected, Connected }
 internal class TcpServer : Server
 {
     public static int Port => 2339;
+    public static string LineEnd => "\r\n";
 
     public override bool IsClientConnected => _server.ConnectionCount > 0;
 
@@ -21,11 +21,11 @@ internal class TcpServer : Server
 
     public TcpServer()
     {
-        _server = new TcpNETServer(new ParamsTcpServer(Port, "\r\n", null, false, 0));
-        _server.ConnectionEvent += Server_ConnectionEvent; ;
+        _server = new TcpNETServer(new ParamsTcpServer(Port, LineEnd, pingIntervalSec: 0));
+        _server.ConnectionEvent += Server_ConnectionEvent;
         _server.MessageEvent += Server_MessageEvent;
-        _server.ErrorEvent += (s, e) => ScreenLogger.Print($"[MlServer] error: {e.Message}"); ;
-        _server.ServerEvent += (s, e) => ScreenLogger.Print($"[MlServer] event: {e.ServerEventType}"); ;
+        _server.ErrorEvent += (s, e) => ScreenLogger.Print($"[MlServer] error: {e.Message}");
+        _server.ServerEvent += (s, e) => ScreenLogger.Print($"[MlServer] event: {e.ServerEventType}");
         _server.StartAsync();
     }
 
@@ -65,67 +65,4 @@ internal class TcpServer : Server
         _client = status == Status.Connected ? args.Connection : null;
         StatusChanged?.Invoke(this, status);
     }
-
-    /*
-    public override bool IsClientConnected => _client != Guid.Empty;
-
-    public TcpServer()
-    {
-        _server = new WatsonTcpServer("0.0.0.0", Port);
-        _server.Events.ClientConnected += ClientConnected;
-        _server.Events.ClientDisconnected += ClientDisconnected;
-        _server.Events.MessageReceived += MessageReceived;
-        _server.Events.ExceptionEncountered += (s, e) => ScreenLogger.Print("[MlServer] error: " + e.Exception.ToString());
-        _server.Events.StreamReceived += (s, e) =>
-        {
-            var a = Encoding.UTF8.GetString(e.Data);
-            ScreenLogger.Print("[MlServer] stream: " + a);
-        };
-        _server.Callbacks.SyncRequestReceived = SyncRequestReceived;
-        _server.Start();
-    }
-
-    public override void Dispose()
-    {
-        _server.Stop();
-        _server.Dispose();
-        GC.SuppressFinalize(this);
-    }
-
-    // Internal
-
-    readonly WatsonTcpServer _server;
-
-    Guid _client = Guid.Empty;
-
-    protected override async Task SendTextAsync(string data)
-    {
-        await _server.SendAsync(_client, data);
-    }
-
-    private void ClientConnected(object? sender, ConnectionEventArgs args)
-    {
-        _client = args.Client.Guid;
-        ScreenLogger.Print("[MlServer] connected: " + args.Client.ToString());
-        StatusChanged?.Invoke(this, Status.Connected);
-    }
-
-    private void ClientDisconnected(object? sender, DisconnectionEventArgs args)
-    {
-        _client = Guid.Empty;
-        ScreenLogger.Print("[MlServer] disconnected: " + args.Client.ToString() + ": " + args.Reason.ToString());
-        StatusChanged?.Invoke(this, Status.Disconnected);
-    }
-
-    private void MessageReceived(object? sender, MessageReceivedEventArgs args)
-    {
-        var json = Encoding.UTF8.GetString(args.Data);
-        ParseJson(json);
-    }
-
-    private SyncResponse SyncRequestReceived(SyncRequest req)
-    {
-        return new SyncResponse(req, "Ack Sync");
-    }
-    */
 }
--- smop.ml/TcpSimulator.cs
+++ smop.ml/TcpSimulator.cs
@@ -3,7 +3,6 @@ using System.Text;
 using System.Threading.Tasks;
 using Tcp.NET.Client;
 using Tcp.NET.Client.Models;
-//using WatsonTcp;
 
 namespace Smop.ML;
 
@@ -11,7 +10,7 @@ internal class TcpSimulator : Simulator
 {
     public TcpSimulator()
     {
-        _client = new TcpNETClient(new ParamsTcpClient("localhost", TcpServer.Port, "\r\n", isSSL: false));
+        _client = new TcpNETClient(new ParamsTcpClient("localhost", TcpServer.Port, TcpServer.LineEnd, isSSL: false));
         _client.MessageEvent += Client_MessageEvent;
         _client.ConnectionEvent += (s, e) => ScreenLogger.Print($"[MlSimul] {e.ConnectionEventType}");
         _client.ErrorEvent += (s, e) => ScreenLogger.Print($"[MlSimul] error: {e.Message}"); ;
@@ -42,51 +41,4 @@ internal class TcpSimulator : Simulator
             ParseJson(json);
         }
     }
-
-    /*
-    public TcpSimulator()
-    {
-        _client = new WatsonTcpClient("127.0.0.1", TcpServer.Port);
-        _client.Events.ServerConnected += ServerConnected;
-        _client.Events.ServerDisconnected += ServerDisconnected;
-        _client.Events.MessageReceived += MessageReceived;
-        _client.Callbacks.SyncRequestReceived = SyncRequestReceived;
-        _client.Connect();
-    }
-
-    public override void Dispose()
-    {
-        _client.Dispose();
-    }
-
-
-    // Internal
-
-    readonly WatsonTcpClient _client;
-
-    protected override async Task SendData(string data)
-    {
-        await _client.SendAsync(data);
-    }
-
-    private void MessageReceived(object? sender, MessageReceivedEventArgs args)
-    {
-        string json = Encoding.UTF8.GetString(args.Data);
-        ParseJson(json);
-    }
-
-    private void ServerConnected(object? sender, ConnectionEventArgs args)
-    {
-        ScreenLogger.Print("[MlSimul] connected");
-    }
-
-    private void ServerDisconnected(object? sender, DisconnectionEventArgs args)
-    {
-        ScreenLogger.Print("[MlSimul] disconnected");
-    }
-
-    private SyncResponse SyncRequestReceived(SyncRequest req)
-    {
-        return new SyncResponse(req, "Ack Sync");
-    }*/
 }

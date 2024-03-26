1c2ff6a3814582c0979688e7192ee405c3d5e954
lexasss <oleg.spakov@tuni.fi>
2024-03-26 23:04:24 +0200

    fine SNT data emulation

--- smop.smell-insp/Data.cs
+++ smop.smell-insp/Data.cs
@@ -39,7 +39,30 @@ public record class Data(float[] Resistances, float Temperature, float Humidity)
         temperature /= samples.Count;
         humidity /= samples.Count;
 
-        return new(resistances, temperature, humidity);
+        return new Data(resistances, temperature, humidity).AsFeatures();
+    }
+
+    public Data AsFeatures()
+    {
+        return new Data(new float[]
+            {
+                (Resistances[11] + Resistances[12] + Resistances[13]) / 3,
+                (Resistances[8] + Resistances[9] + Resistances[10]) / 3,
+                (Resistances[5] + Resistances[6] + Resistances[7]) / 3,
+                (Resistances[43] + Resistances[44] + Resistances[45]) / 3,
+                (Resistances[27] + Resistances[28] + Resistances[29]) / 3,
+                (Resistances[24] + Resistances[25] + Resistances[26]) / 3,
+                (Resistances[40] + Resistances[41] + Resistances[42]) / 3,
+                (Resistances[59] + Resistances[60] + Resistances[61]) / 3,
+                (Resistances[21] + Resistances[22] + Resistances[23]) / 3,
+                (Resistances[37] + Resistances[38] + Resistances[39]) / 3,
+                (Resistances[2] + Resistances[3] + Resistances[4]) / 3,
+                (Resistances[56] + Resistances[57] + Resistances[58]) / 3,
+                (Resistances[53] + Resistances[54] + Resistances[55]) / 3,
+                (Resistances[34] + Resistances[35] + Resistances[36]) / 3,
+                (Resistances[50] + Resistances[51] + Resistances[52]) / 3,
+            }, Temperature, Humidity
+        );
     }
 }
 
--- smop.smell-insp/SerialPortEmulator.cs
+++ smop.smell-insp/SerialPortEmulator.cs
@@ -6,9 +6,9 @@ namespace Smop.SmellInsp;
 
 public static class SimulatedData
 {
-    public record class SimulatedResponseGains(float First, float Second);
+    public record class GasImpact(float First, float Second);
 
-    public static SimulatedResponseGains Gains = new(8, 6);
+    public static GasImpact Gains { get; set; } = new(8, 6);
 
     public static Data Generate()
     {
@@ -17,20 +17,21 @@ public static class SimulatedData
         {
             resistance[i] = i switch
             {
-                1 => 0.1f * Gains.First + Random.Range(0.3f),
-                5 => 0.35f * Gains.First + Random.Range(0.3f),
-                9 => 0.8f * Gains.First + Random.Range(0.3f),
-                13 => 1.0f * Gains.First + Random.Range(0.3f),
-
-                2 => 0.1f * Gains.Second + Random.Range(0.3f),
-                6 => 0.35f * Gains.Second + Random.Range(0.3f),
-                10 => 0.8f * Gains.Second + Random.Range(0.3f),
-                14 => 1.0f * Gains.Second + Random.Range(0.3f),
-
-                0 => 0.05f * Gains.First + 0.05f * Gains.Second + Random.Range(0.3f),
-                4 => 0.1f * Gains.First + 0.05f * Gains.Second + Random.Range(0.3f),
-                8 => 0.1f * Gains.First + 0.2f * Gains.Second + Random.Range(0.3f),
-                12 => 0.5f * Gains.First + 0.5f * Gains.Second + Random.Range(0.3f),
+                >= 2 and <= 4 => _impacts.Gas1[10] * Gains.First + _impacts.Gas2[10] * Gains.Second + Random.Range(0.3f) + TS(100),
+                >= 5 and <= 7 => _impacts.Gas1[2] * Gains.First + _impacts.Gas2[2] * Gains.Second + Random.Range(0.3f) + TS(10),
+                >= 8 and <= 10 => _impacts.Gas1[1] * Gains.First + _impacts.Gas2[1] * Gains.Second + Random.Range(0.3f) + TS(3),
+                >= 11 and <= 13 => _impacts.Gas1[0] * Gains.First + _impacts.Gas2[0] * Gains.Second + Random.Range(0.3f) + TS(30),
+                >= 21 and <= 23 => _impacts.Gas1[8] * Gains.First + _impacts.Gas2[8] * Gains.Second + Random.Range(0.3f) + TS(40),
+                >= 24 and <= 26 => _impacts.Gas1[5] * Gains.First + _impacts.Gas2[5] * Gains.Second + Random.Range(0.3f) + TS(500),
+                >= 27 and <= 29 => _impacts.Gas1[4] * Gains.First + _impacts.Gas2[4] * Gains.Second + Random.Range(0.3f) + TS(30),
+                >= 34 and <= 36 => _impacts.Gas1[13] * Gains.First + _impacts.Gas2[13] * Gains.Second + Random.Range(0.3f) + TS(10),
+                >= 37 and <= 39 => _impacts.Gas1[9] * Gains.First + _impacts.Gas2[9] * Gains.Second + Random.Range(0.3f) + TS(2),
+                >= 40 and <= 42 => _impacts.Gas1[6] * Gains.First + _impacts.Gas2[6] * Gains.Second + Random.Range(0.3f) + TS(200),
+                >= 43 and <= 45 => _impacts.Gas1[3] * Gains.First + _impacts.Gas2[3] * Gains.Second + Random.Range(0.3f) + TS(6),
+                >= 50 and <= 52 => _impacts.Gas1[14] * Gains.First + _impacts.Gas2[14] * Gains.Second + Random.Range(0.3f) + TS(20),
+                >= 53 and <= 55 => _impacts.Gas1[12] * Gains.First + _impacts.Gas2[12] * Gains.Second + Random.Range(0.3f) + TS(30),
+                >= 56 and <= 58 => _impacts.Gas1[11] * Gains.First + _impacts.Gas2[11] * Gains.Second + Random.Range(0.3f) + TS(100),
+                >= 59 and <= 61 => _impacts.Gas1[7] * Gains.First + _impacts.Gas2[7] * Gains.Second + Random.Range(0.3f) + TS(20),
 
                 _ => 4.6f + Random.Range(0.3f)
             };
@@ -42,11 +43,28 @@ public static class SimulatedData
         );
     }
 
+    // internal
+
     private static class Random
     {
         public static float Range(float pm) => (float)((_random.NextDouble() - 0.5) * 2 * pm);
 
         static readonly System.Random _random = new();
+    }
+
+    private record class FeatureImpact(float[] Gas1, float[] Gas2);
+
+    static readonly FeatureImpact _impacts = new(
+        new float[15] { 0.1f, 0.3f, 0f, 0f, 0.9f, 1f, 0.1f, 0f, 0.2f, 0f, 0f, 0.6f, 0f, 0.8f, 0f},
+        new float[15] { 0f, 0.1f, 0f, 0.8f, 0.4f, 0f, 0.3f, 0f, 0f, 0.8f, 1f, 0.2f, 0f, 0f, 0.5f}
+    );
+
+    static readonly long _start = DateTime.Now.Ticks;
+
+    private static float TS(double gain)
+    {
+        var x = (double)(DateTime.Now.Ticks - _start) / 10_000_000;  // half-period is 1 second
+        return (float)(gain * x / Math.Sqrt(1 + x * x));
     }
 }
 
